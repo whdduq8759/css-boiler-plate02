93d59fd4baaea0fb88709e46fca9863dc5cfeb3f
kokokaki <hsg9984@kitt.co.kr>
2021-03-11 13:06:37 +0900

    [21.03.11] # 맨위로 이동 부드럽게 처리 기능 추가

--- js/scroll-event.js
+++ js/scroll-event.js
@@ -48,4 +48,13 @@ function changeFixHeader(scrollTop) {
         
     });
 
+
+    //go-top 스크롤 부드럽게 처리
+    document.querySelector('.go-top').addEventListener('click', e => {
+        //jQuery animate함수: 애니메이션의 종류나 속도 지정
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1200);
+    });
+
 }());
